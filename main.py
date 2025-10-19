908164786e8893715b9f513faf7ec289f07edc9f
VincentWilson <v1wilson@student.bridgew.edu>
2025-10-19 14:43:24 -0400

    Bar Graphs are visible

--- main.py
+++ main.py
@@ -1,32 +1,38 @@
 import dearpygui.dearpygui as graphics
 import comp151Colors
-# ["Minnesota", "Alaska", "Oregon", "Wisconsin", "Ohio"]
 bar_width = 100
 next_bar_start = 50
 states_info = open("Project4.txt", "r")
+v_states = ["Minnesota", "Alaska", "Oregon", "Wisconsin", "Ohio"]
 info = states_info.readlines()
 graphics.create_context()
-graphics.create_viewport(title="Income Graph", width=900, height=900)
-with graphics.window(label="Info Graph", width=900, height=900):
-    with graphics.drawlist(width=900, height=900):
-        graphics.draw_arrow((30, 10), (30, 790), color=comp151Colors.LIGHT_BLUE, thickness=5)
-        graphics.draw_arrow((790, 800), (30, 800), color=comp151Colors.TEAL, thickness=5)
-        graphics.draw_text((0, 100), "800k", size=13)
-        graphics.draw_text((0, 200), "700k", size=13)
-        graphics.draw_text((0, 300), "600k", size=13)
-        graphics.draw_text((0, 400), "500k", size=13)
-        graphics.draw_text((0, 500), "400k", size=13)
-        graphics.draw_text((0, 600), "300k", size=13)
-        graphics.draw_text((0, 700), "200k", size=13)
-        graphics.draw_text((0, 800), "100k", size=13)
+graphics.create_viewport(title="Income Graph", width=1000, height=1800)
+with graphics.window(label="Info Graph", width=800, height=1000):
+    with graphics.drawlist(width=800, height=1100):
+        graphics.draw_arrow((30, 10), (30, 900), color=comp151Colors.TEAL, thickness=5)
+        graphics.draw_arrow((790, 920), (30, 920), color=comp151Colors.TEAL, thickness=5)
+        graphics.draw_text((0, 10), "900k", size=13)
+        graphics.draw_text((0, 110), "800k", size=13)
+        graphics.draw_text((0, 210), "700k", size=13)
+        graphics.draw_text((0, 310), "600k", size=13)
+        graphics.draw_text((0, 410), "500k", size=13)
+        graphics.draw_text((0, 510), "400k", size=13)
+        graphics.draw_text((0, 610), "300k", size=13)
+        graphics.draw_text((0, 710), "200k", size=13)
+        graphics.draw_text((0, 810), "100k", size=13)
+        graphics.draw_text((0, 910), "0k", size=13)
+
+
     for line in info:
-            line_info = line.split(";")
-            median_house_price = float(line_info[5])
-            bar_height = median_house_price/1000
-            bar_color= comp151Colors.BLUE
-            graphics.draw_rectangle((next_bar_start, 0), (next_bar_start+bar_width, bar_height),
+        line_info = line.split(";")
+        name_state = line_info[0].strip()
+        median_house_price = float(line_info[5])
+        bar_height = median_house_price/1000
+        bar_color= comp151Colors.BLUE
+        graphics.draw_rectangle((next_bar_start, 910-bar_height), (next_bar_start+bar_width, 910),
                                fill=bar_color)
-            next_bar_start += bar_width+30
+        graphics.draw_text((next_bar_start + 10, 920), name_state, size=13)
+        next_bar_start += bar_width+30
 
 graphics.setup_dearpygui()
 graphics.show_viewport()
